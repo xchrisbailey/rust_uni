1a4cd80137312361838cd104624ec975f0d6b371
Chris Bailey <chris@ghostly.io>
2024-04-04 18:33:47 -0400

    ✨ (main.rs): add setup_database function to create a task table in the database when the application starts

--- todo/src/main.rs
+++ todo/src/main.rs
@@ -1,6 +1,12 @@
 use clap::Parser;
+use rusqlite::Result;
 
 fn main() {
+    if let Err(e) = setup_database() {
+        eprintln!("Error setting up database: {}", e);
+        std::process::exit(1);
+    }
+
     let cli = Cli::parse();
     if let Some(task) = cli.task.as_deref() {
         println!("{}", task);
@@ -11,4 +17,19 @@ fn main() {
 #[command(version, about, long_about = None)]
 struct Cli {
     task: Option<String>,
+}
+
+fn setup_database() -> Result<()> {
+    let conn = rusqlite::Connection::open("./tasks.db")?;
+
+    conn.execute(
+        "CREATE TABLE task (
+id INTEGER PRIMARY KEY,
+task TEXT NOT NULL,
+done BOOLEAN NOT NULL DEFAULT 0
+)",
+        [],
+    )?;
+
+    Ok(())
 }
