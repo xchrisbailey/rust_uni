17e4ccb879260b6e8cc1f43a97c0804733e01964
Chris Bailey <chris@ghostly.io>
2024-04-04 18:21:52 -0400

    ✨ (main.rs): introduce clap crate to parse command line arguments for the CLI application 📝 (main.rs): add a struct Cli with a task field to represent the command line arguments for the application

--- todo/src/main.rs
+++ todo/src/main.rs
@@ -1,3 +1,14 @@
+use clap::Parser;
+
 fn main() {
-    println!("Hello, world!");
+    let cli = Cli::parse();
+    if let Some(task) = cli.task.as_deref() {
+        println!("{}", task);
+    }
+}
+
+#[derive(Parser)]
+#[command(version, about, long_about = None)]
+struct Cli {
+    task: Option<String>,
 }
