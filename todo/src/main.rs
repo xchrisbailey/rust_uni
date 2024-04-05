14452a1476d2a20d2962b1204c4f6d7afd9be95c
Chris Bailey <chris@ghostly.io>
2024-04-04 22:52:20 -0400

    ✨ (main.rs): Add support for subcommands in CLI to handle different actions like adding tasks 📝 (main.rs): Update database setup function to return a connection object for better error handling and reusability

--- todo/src/main.rs
+++ todo/src/main.rs
@@ -1,36 +1,52 @@
-use clap::Parser;
-use rusqlite::Result;
+use clap::{Parser, Subcommand};
+use rusqlite::{Connection, Result};
 
-fn main() {
-    if let Err(e) = setup_database() {
-        eprintln!("Error setting up database: {}", e);
-        std::process::exit(1);
-    }
+fn main() -> Result<()> {
+    let conn = match setup_database() {
+        Ok(conn) => conn,
+        Err(e) => {
+            eprintln!("Error setting up database: {}", e);
+            return Ok(());
+        }
+    };
 
     let cli = Cli::parse();
-    if let Some(task) = cli.task.as_deref() {
-        println!("{}", task);
+
+    match &cli.command {
+        Commands::Add { task } => {
+            conn.execute(
+                "INSERT INTO tasks (task, status) VALUES (?1, ?2)",
+                (task, 0),
+            )?;
+        }
     }
+
+    Ok(())
 }
 
 #[derive(Parser)]
 #[command(version, about, long_about = None)]
 struct Cli {
-    task: Option<String>,
+    #[command(subcommand)]
+    command: Commands,
+}
+
+#[derive(Subcommand)]
+enum Commands {
+    Add { task: String },
 }
 
 // Create a new database and table for tasks
-fn setup_database() -> Result<()> {
+fn setup_database() -> Result<Connection> {
     let conn = rusqlite::Connection::open("./tasks.db")?;
-
     conn.execute(
-        "CREATE TABLE task (
+        "CREATE TABLE IF NOT EXISTS tasks (
 id INTEGER PRIMARY KEY,
 task TEXT NOT NULL,
-done BOOLEAN NOT NULL DEFAULT 0
+status BOOLEAN NOT NULL DEFAULT 0
 )",
         [],
     )?;
 
-    Ok(())
+    Ok(conn)
 }
