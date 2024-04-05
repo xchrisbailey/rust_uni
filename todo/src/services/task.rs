3e7f8c0e8f77525abd5d90b9e39cdb15305ac1d6
Chris Bailey <chris@ghostly.io>
2024-04-05 01:05:59 -0400

    ⬆️ (Cargo.toml): upgrade comfy-table dependency to version 7.1.0 ♻️ (task.rs): refactor code to use comfy-table for displaying tasks in a table format and improve readability of the code 📝 (task.rs): add documentation comments to functions and struct for better code understanding

--- todo/src/services/task.rs
+++ todo/src/services/task.rs
@@ -1,7 +1,8 @@
-use rusqlite::{Connection, Result};
+use comfy_table::{Cell, Row, Table};
+use rusqlite::Connection;
 
 // Add a task to the database
-pub fn add_task(conn: &Connection, task: &str) -> Result<()> {
+pub fn add_task(conn: &Connection, task: &str) -> rusqlite::Result<()> {
     conn.execute(
         "INSERT INTO tasks (task, status) VALUES (?1, ?2)",
         (task, 0),
@@ -10,19 +11,19 @@ pub fn add_task(conn: &Connection, task: &str) -> Result<()> {
 }
 
 // Delete a task from the database
-pub fn delete_task(conn: &Connection, id: i32) -> Result<()> {
+pub fn delete_task(conn: &Connection, id: i32) -> rusqlite::Result<()> {
     conn.execute("DELETE FROM tasks WHERE id = ?1", [id])?;
     Ok(())
 }
 
 // Mark a task as complete
-pub fn complete_task(conn: &Connection, id: i32) -> Result<()> {
+pub fn complete_task(conn: &Connection, id: i32) -> rusqlite::Result<()> {
     conn.execute("UPDATE tasks SET status = 1 WHERE id = ?1", [id])?;
     Ok(())
 }
 
 // Get all tasks from the database
-pub fn get_all_tasks(conn: &Connection) -> Result<()> {
+pub fn get_all_tasks(conn: &Connection) -> rusqlite::Result<()> {
     let mut stmt = conn.prepare("SELECT * FROM tasks")?;
     let task_iter = stmt.query_map([], |row| {
         Ok(Task {
@@ -31,14 +32,31 @@ pub fn get_all_tasks(conn: &Connection) -> Result<()> {
             status: row.get(2)?,
         })
     })?;
+
+    let mut tasks: Vec<comfy_table::Row> = Vec::new();
+
     for task in task_iter {
-        task?.print();
+        let Task { id, task, status } = task.unwrap();
+        let value = vec![
+            Cell::new(id),
+            Cell::new(task),
+            Cell::new(match status {
+                true => "finsihed",
+                false => "pending",
+            }),
+        ];
+        tasks.push(Row::from(value));
     }
+    let mut table = Table::new();
+    table
+        .set_header(vec!["ID", "Task", "Status"])
+        .add_rows(tasks);
+    println!("{}", table);
     Ok(())
 }
 
 // Find single task by id
-pub fn get_task_by_id(conn: &Connection, id: i32) -> Result<()> {
+pub fn get_task_by_id(conn: &Connection, id: i32) -> rusqlite::Result<()> {
     let mut stmt = conn.prepare("SELECT * FROM tasks WHERE id = ?1")?;
     let task = stmt.query_row([id], |row| {
         Ok(Task {
@@ -51,7 +69,7 @@ pub fn get_task_by_id(conn: &Connection, id: i32) -> Result<()> {
     Ok(())
 }
 
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 struct Task {
     id: i32,
     task: String,
