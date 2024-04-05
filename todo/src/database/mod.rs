883e50c87547117d3481474f9e02a3cd655cd311
Chris Bailey <chris@ghostly.io>
2024-04-04 23:16:12 -0400

    ✨ (database/mod.rs): create a new module to handle database operations and setup a database with a tasks table 🔧 (main.rs): refactor main.rs to use the setup_database function from the new database module for setting up the database and tasks table, removing duplicate code.

---
+++ todo/src/database/mod.rs
@@ -0,0 +1,16 @@
+use rusqlite::{Connection, Result};
+
+// Create a new database and table for tasks
+pub fn setup_database() -> Result<Connection> {
+    let conn = rusqlite::Connection::open("./tasks.db")?;
+    conn.execute(
+        "CREATE TABLE IF NOT EXISTS tasks (
+id INTEGER PRIMARY KEY,
+task TEXT NOT NULL,
+status BOOLEAN NOT NULL DEFAULT 0
+)",
+        [],
+    )?;
+
+    Ok(conn)
+}
--- todo/src/main.rs
+++ todo/src/main.rs
@@ -1,7 +1,9 @@
+mod database;
 mod services;
 
 use clap::{Parser, Subcommand};
-use rusqlite::{Connection, Result};
+use database::setup_database;
+use rusqlite::Result;
 use services::task::{add_task, complete_task, delete_task, get_all_tasks, get_task_by_id};
 
 fn main() -> Result<()> {
@@ -40,19 +42,4 @@ enum Commands {
     Complete { id: i32 },
     All,
     View { id: i32 },
-}
-
-// Create a new database and table for tasks
-fn setup_database() -> Result<Connection> {
-    let conn = rusqlite::Connection::open("./tasks.db")?;
-    conn.execute(
-        "CREATE TABLE IF NOT EXISTS tasks (
-id INTEGER PRIMARY KEY,
-task TEXT NOT NULL,
-status BOOLEAN NOT NULL DEFAULT 0
-)",
-        [],
-    )?;
-
-    Ok(conn)
 }
