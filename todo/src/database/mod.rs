16ee53b9908a0556a1e1c44a702305b44e0be1ba
Chris Bailey <chris@ghostly.io>
2024-04-06 10:09:32 -0400

    🔧 (Cargo.toml): Add homedir dependency to enable getting the user's home directory path for the database file 🔧 (database/mod.rs): Use homedir crate to get the user's home directory path and create the database file in the user's home directory 🔧 (database/mod.rs): Refactor code to handle getting the database path and creating the database file in a more structured way

--- todo/src/database/mod.rs
+++ todo/src/database/mod.rs
@@ -1,8 +1,18 @@
+use homedir::get_my_home;
 use rusqlite::{Connection, Result};
 
 // Create a new database and table for tasks
 pub fn setup_database() -> Result<Connection> {
-    let conn = rusqlite::Connection::open("./tasks.db")?;
+    let conn: Connection;
+
+    match get_db_path() {
+        Ok(path) => conn = rusqlite::Connection::open(path)?,
+        Err(e) => {
+            println!("Error getting database path: {}", e);
+            std::process::exit(1)
+        }
+    }
+
     conn.execute(
         "CREATE TABLE IF NOT EXISTS tasks (
 id INTEGER PRIMARY KEY,
@@ -13,4 +23,16 @@ status BOOLEAN NOT NULL DEFAULT 0
     )?;
 
     Ok(conn)
+}
+
+// Get the path to the database
+fn get_db_path() -> Result<String, std::io::Error> {
+    if let Ok(path) = get_my_home() {
+        Ok(format!("{}/task.db", path.unwrap().display()))
+    } else {
+        Err(std::io::Error::new(
+            std::io::ErrorKind::Other,
+            "Error getting home directory",
+        ))
+    }
 }
