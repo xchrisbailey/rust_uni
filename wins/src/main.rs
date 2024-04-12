f84ddeb0bb8a21b17fda1c6b54d7d9866d123a77
Chris Bailey <chris@ghostly.io>
2024-04-12 09:11:58 -0400

    ✨ (Cargo.toml): Add tower and tower-http dependencies to the project 📝 (main.rs): Update main.rs to use askama for template rendering and add a new route for index page 📝 (main.rs): Implement index template rendering logic and error handling for template rendering 📝 (index.html): Add a new index.html template file with a simple heading "Hi"

--- wins/src/main.rs
+++ wins/src/main.rs
@@ -1,6 +1,13 @@
 use std::net::SocketAddr;
 
-use axum::{routing::get, Router};
+use askama::Template;
+use axum::{
+    http::StatusCode,
+    response::{Html, IntoResponse, Response},
+    routing::get,
+    Router,
+};
+use tracing::info;
 use tracing_subscriber::{layer::SubscriberExt, util::SubscriberInitExt};
 
 #[tokio::main]
@@ -13,10 +20,39 @@ async fn main() {
         .with(tracing_subscriber::fmt::layer())
         .init();
 
-    let app = Router::new().route("/", get(|| async { "Hellow World" }));
+    info!("starting router");
+
+    let app = Router::new().route("/", get(index));
 
     let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
     tracing::debug!("listening on {addr}");
     let listener = tokio::net::TcpListener::bind(addr).await.unwrap();
     axum::serve(listener, app).await.unwrap();
+}
+
+async fn index() -> impl IntoResponse {
+    let template = IndexTemplate {};
+    HtmlTemplate(template)
+}
+
+#[derive(Template)]
+#[template(path = "index.html")]
+struct IndexTemplate;
+
+struct HtmlTemplate<T>(T);
+
+impl<T> IntoResponse for HtmlTemplate<T>
+where
+    T: Template,
+{
+    fn into_response(self) -> Response {
+        match self.0.render() {
+            Ok(html) => Html(html).into_response(),
+            Err(err) => (
+                StatusCode::INTERNAL_SERVER_ERROR,
+                format!("Failed rendering template. Error: {}", err),
+            )
+                .into_response(),
+        }
+    }
 }
