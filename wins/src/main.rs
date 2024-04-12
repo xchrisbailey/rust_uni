0f86b0b75543a607f33bac7067cd6b9200426f37
Chris Bailey <chris@ghostly.io>
2024-04-12 01:43:02 -0400

    📝 (main.rs): add tracing subscriber to enable logging and set log level based on environment variable for better debugging and monitoring

--- wins/src/main.rs
+++ wins/src/main.rs
@@ -1,9 +1,18 @@
 use std::net::SocketAddr;
 
 use axum::{routing::get, Router};
+use tracing_subscriber::{layer::SubscriberExt, util::SubscriberInitExt};
 
 #[tokio::main]
 async fn main() {
+    tracing_subscriber::registry()
+        .with(
+            tracing_subscriber::EnvFilter::try_from_default_env()
+                .unwrap_or_else(|_| "wins=debug".into()),
+        )
+        .with(tracing_subscriber::fmt::layer())
+        .init();
+
     let app = Router::new().route("/", get(|| async { "Hellow World" }));
 
     let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
