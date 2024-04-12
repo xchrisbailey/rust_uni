2eae63da58747cc25f429948e5c69c07ffd7bb0f
Chris Bailey <chris@ghostly.io>
2024-04-12 01:39:38 -0400

    ⬆️ (Cargo.toml): upgrade tracing to version 0.1 and tracing-subscriber to version 0.3 with env-filter feature ♻️ (main.rs): refactor main function to use axum Router for routing and add tracing for debugging purposes

--- wins/src/main.rs
+++ wins/src/main.rs
@@ -1,3 +1,13 @@
-fn main() {
-    println!("Hello, world!");
+use std::net::SocketAddr;
+
+use axum::{routing::get, Router};
+
+#[tokio::main]
+async fn main() {
+    let app = Router::new().route("/", get(|| async { "Hellow World" }));
+
+    let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
+    tracing::debug!("listening on {addr}");
+    let listener = tokio::net::TcpListener::bind(addr).await.unwrap();
+    axum::serve(listener, app).await.unwrap();
 }
