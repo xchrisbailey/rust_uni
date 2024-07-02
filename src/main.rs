bf611d78db8f9f4be1f76e0da3c4cedf2601f0f1
Chris Bailey <chris@ghostly.io>
2024-07-02 03:15:05 -0400

    ⬆️ (Cargo.toml): upgrade regex dependency to version 1.10.5 ♻️ (main.rs): refactor main.rs to include a function to match a pattern in files The regex dependency in Cargo.toml has been upgraded to version 1.10.5 to ensure compatibility with the latest features and bug fixes. The main.rs file has been refactored to include a new function, match_pattern, which allows for matching a specified pattern in files and displaying the matching lines. This refactoring improves code readability and maintainability by separating the logic for matching patterns into a reusable function.

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,45 @@
 use std::{
-    env, fs,
-    io::Result,
+    env,
+    fs::{self, File},
+    io::{BufRead, BufReader, Lines, Result},
     path::{Path, PathBuf},
 };
+
+use regex::Regex;
 
 fn main() -> Result<()> {
     let mut args = env::args();
     let binding = args.nth(1).expect("Expected a path argument");
+    let pattern: &str = &args.next().expect("Expected a pattern argument");
     let path = Path::new(&binding);
     let files = collect_files(path)?;
 
     for file in files {
-        println!("{}", file.display());
+        let f = File::open(&file).expect("Could not open file");
+        let buf = BufReader::new(f).lines();
+        let reg = Regex::new(pattern).expect("Invalid pattern");
+        let matches = match_pattern(buf, reg)?;
+        if !matches.is_empty() {
+            println!("File: {:?}", file);
+            for line in matches {
+                println!("{}", line);
+            }
+            println!("\n")
+        }
     }
 
     Ok(())
+}
+
+fn match_pattern(content: Lines<BufReader<File>>, pattern: Regex) -> Result<Vec<String>> {
+    let mut matches = vec![];
+    for (line_num, line) in content.enumerate() {
+        let line_str = line.expect("Could not read line");
+        if pattern.is_match(&line_str) {
+            matches.push(format!("{} - {}", line_num, line_str.trim()));
+        }
+    }
+    Ok(matches)
 }
 
 fn collect_files(dir: &Path) -> Result<Vec<PathBuf>> {
